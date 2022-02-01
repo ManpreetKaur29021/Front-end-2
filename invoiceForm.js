49a0c1ee26943c0448e0862ef779c75d62920354
YASHASVI YADAV ALANI <91046772+yashasvi28@users.noreply.github.com>
2022-02-01 22:03:41 +0530

    Add files via upload

---
+++ invoiceForm.js
@@ -0,0 +1,37 @@
+
+const item=document.getElementsByClassName("item");
+const form1=document.querySelector(".form-1");
+const form2=document.querySelector(".form-2");
+
+
+item[1].style.backgroundColor="#d4d4d5";
+            item[0].style.backgroundColor="#dedee1";
+            
+form1.style.display="block";
+form2.style.display="none";	  
+
+        item[0].onclick=function(){
+            item[1].style.backgroundColor="#d4d4d5";
+            item[0].style.backgroundColor="#dedee1";
+            
+			form1.style.display="block";
+			form2.style.display="none";	  
+		
+        }
+		
+		item[1].onclick=function(){
+			item[1].style.backgroundColor="#dedee1";
+			item[0].style.backgroundColor="#d4d4d5";
+			
+			form2.style.display="block";
+			form1.style.display="none";	  
+			
+		}
+		
+
+        $(".click").click(function(){
+            $(".account").toggle();
+        })
+        $("body").click(function(){
+            $(".account").addClass("invi");
+        })
