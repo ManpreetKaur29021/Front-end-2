eb6c3c86bd0051d518a896e70582dd89963f4d2e
YASHASVI YADAV ALANI <91046772+yashasvi28@users.noreply.github.com>
2022-02-04 21:28:52 +0530

    Add files via upload
    Updates files

--- invoiceForm.js
+++ invoiceForm.js
@@ -3,29 +3,34 @@ const item=document.getElementsByClassName("item");
 const form1=document.querySelector(".form-1");
 const form2=document.querySelector(".form-2");
 
+const button1=document.getElementById("#button1");
+const button2=document.getElementById("#button2");
+const count=document.getElementById("#count");
 
 item[1].style.backgroundColor="#d4d4d5";
             item[0].style.backgroundColor="#dedee1";
-            
+          
 form1.style.display="block";
-form2.style.display="none";	  
+form2.style.display="none";	 
 
         item[0].onclick=function(){
             item[1].style.backgroundColor="#d4d4d5";
             item[0].style.backgroundColor="#dedee1";
-            
 			form1.style.display="block";
-			form2.style.display="none";	  
-		
-        }
+			form2.style.display="none";		
+            button1.style.display="block";
+    button2.style.display="none";  
+    count.style.display="block";
+}
 		
 		item[1].onclick=function(){
 			item[1].style.backgroundColor="#dedee1";
 			item[0].style.backgroundColor="#d4d4d5";
-			
 			form2.style.display="block";
-			form1.style.display="none";	  
-			
+			form1.style.display="none";	
+            button1.style.display="none";
+    button2.style.display="block"; 	
+    count.style.display="none";		
 		}
 		
 
